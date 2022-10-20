152d47d3b146d5ac8079a3e4238938a4b60a396e
GregT <taylor.gregory2@gmail.com>
2022-10-19 22:30:43 -0600

    buttons centered

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import Trainer from './Trainer';
 import Hub from './Hub';
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button, ButtonGroup } from '@mui/material';
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Toolbar, Container, Button, ButtonGroup } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
 import { PhotoCamera } from '@mui/icons-material'
 
 
@@ -24,13 +25,27 @@ function App() {
           <Typography variant='h5' align='center' color='textSecondary' paragraph>
             Hello everyone, this is a photo album and I'm trying to make this sentence as long as possible so that we can see it on the screen. Heck yeah!
           </Typography>
+          <div>
+            <Grid container spacing={2} columns={2} alignItems='center' justifyContent='center'>
+              <Grid>
+                <Button variant='contained' color='primary'>
+                  See My Photos
+                </Button>
+              </Grid>
+              <Grid>
+                <Button variant='outlined' color='primary'>
+                  Close My Photos
+                </Button>
+              </Grid>
+            </Grid>
+          </div>
         </Container>
       </div>
-      <ButtonGroup variant="contained" aria-label="outlined primary button group">
+      {/* <ButtonGroup variant="contained" aria-label="outlined primary button group">
       <Button>One</Button>
       <Button>Two</Button>
       <Button>Three</Button>
-    </ButtonGroup>
+    </ButtonGroup> */}
     </main>
     </>
   );
