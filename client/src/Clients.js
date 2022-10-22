1dfbc852a7b4e19ac834d8d372ce91b58ef98d99
GregT <taylor.gregory2@gmail.com>
2022-10-21 22:01:10 -0600

    doughnut chart started

--- client/src/Clients.js
+++ client/src/Clients.js
@@ -1,12 +1,30 @@
 import { CardContent, Typography, Card } from "@mui/material";
 import Grid from '@mui/material/Unstable_Grid2'
 import { useEffect, useState } from "react";
+import { Doughnut } from 'react-chartjs-2'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 
 
 function Clients(){
 
     const [clientCount, setClientCount] = useState([])
+
+    const data = {
+        datasets: [
+          {
+            data: [63, 15, 22],
+            backgroundColor: ['#3F51B5', '#e53935', '#FB8C00'],
+            borderWidth: 8,
+            borderColor: '#FFFFFF',
+            hoverBorderColor: '#FFFFFF'
+          }
+        ],
+        labels: ['Desktop', 'Tablet', 'Mobile']
+      };
+    
 
     useEffect(() => {
         fetch('/clients')
@@ -32,7 +50,9 @@ function Clients(){
                     </Grid>
                 </Grid>
             </CardContent>
+            <Doughnut data={data} />
         </Card>
+        
     )
 }
 
