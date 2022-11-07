dc446548ad9881903102bdfe01f6953fa9916472
GregT <taylor.gregory2@gmail.com>
2022-11-07 16:59:03 -0700

    password validation updated

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,5 +1,7 @@
 class Trainer < ApplicationRecord
     has_secure_password
+    validates :password, :length => {:within => 6..40}
+
     has_many :workouts
     has_many :clients, through: :workouts
     has_many :comments
